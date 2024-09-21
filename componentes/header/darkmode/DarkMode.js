dfd9f00cfd574b45da12705ddeed5dae47c5c414
GGElorduy <gilguijarro.elorduy@gmail.com>
2024-09-21 22:41:07 +0200

    Update

--- componentes/header/darkmode/DarkMode.js
+++ componentes/header/darkmode/DarkMode.js
@@ -7,10 +7,12 @@ export const toggle = () => {
   document.body.classList.toggle('dark')
 
   if (toggleIcon.src.includes('moon_pbnfvc9aiadf')) {
-    toggleIcon.src = '#'
+    toggleIcon.src =
+      'https://res.cloudinary.com/dbnbfpype/image/upload/v1726942244/Iconos/sun_i4m40gfcuzly_ro58bl.svg'
     toggleText.textContent = 'Light Mode'
   } else {
-    toggleIcon.src = '#'
+    toggleIcon.src =
+      'https://res.cloudinary.com/dbnbfpype/image/upload/v1726942243/Iconos/moon_pbnfvc9aiadf_trrx0d.svg'
     toggleText.textContent = 'Dark Mode'
   }
 }

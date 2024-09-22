c77c9ba1744a61753dd3edb4678de4c7ac652a73
GGElorduy <gilguijarro.elorduy@gmail.com>
2024-09-22 10:34:49 +0200

    Update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //import './style.css'
-import { toggle } from './componentes/header/darkmode/DarkMode.js'
-
+/* import { toggle } from './componentes/header/darkmode/DarkMode.js'
+import { changeLanguage } from './componentes/header/ChangeLanguage/ChangeLanguage.js'
+ */
 const toggleTheme = document.getElementById('toggle-theme')
 const toggleIcon = document.getElementById('toggle-icon')
 const toggleText = document.getElementById('toggle-text')
@@ -9,7 +10,34 @@ const flagsElement = document.getElementById('flags')
 
 const textsToChange = document.querySelectorAll('[data-section]')
 
-const changeLanguage = async (language) => {
+/* Dark Mode */
+
+export const toggle = () => {
+  const toggleIcon = document.getElementById('toggle-icon')
+  const toggleText = document.getElementById('toggle-text')
+  document.body.classList.toggle('dark')
+
+  if (toggleIcon.src.includes('moon_pbnfvc9aiadf')) {
+    toggleIcon.src =
+      'https://res.cloudinary.com/dbnbfpype/image/upload/v1726942244/Iconos/sun_i4m40gfcuzly_ro58bl.svg'
+    toggleText.textContent = 'Light Mode'
+  } else {
+    toggleIcon.src =
+      'https://res.cloudinary.com/dbnbfpype/image/upload/v1726942243/Iconos/moon_pbnfvc9aiadf_trrx0d.svg'
+    toggleText.textContent = 'Dark Mode'
+  }
+}
+
+toggleTheme.addEventListener('click', () => toggle())
+/* Primary Color */
+const toggleColors = document.getElementById('toggle-colors')
+const rootStyes = document.documentElement.style
+toggleColors.addEventListener('click', (e) => {
+  rootStyes.setProperty('--primary-color', e.target.dataset.color)
+})
+/*  Change Language*/
+
+export const changeLanguage = async (language) => {
   const languageJSONpath = `./languages/${language}.json`
   const requestJson = await fetch(languageJSONpath)
   const text = await requestJson.json()
@@ -23,13 +51,3 @@ const changeLanguage = async (language) => {
 flagsElement.addEventListener('click', (e) => {
   changeLanguage(e.target.parentElement.dataset.language)
 })
-
-/* Dark Mode */
-toggleTheme.addEventListener('click', () => toggle())
-/* Primary Color */
-const toggleColors = document.getElementById('toggle-colors')
-const rootStyes = document.documentElement.style
-toggleColors.addEventListener('click', (e) => {
-  rootStyes.setProperty('--primary-color', e.target.dataset.color)
-})
-/*  */
